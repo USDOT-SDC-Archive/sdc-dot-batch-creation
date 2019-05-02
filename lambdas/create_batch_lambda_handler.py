e88633dacb9daa9303369688e3ec1e71eaed0938
Santosh Karla <santosh.karla@gmail.com>
2019-05-02 13:39:09 -0400

    Add unit tests, ci definition and sonar

--- lambdas/create_batch_lambda_handler.py
+++ lambdas/create_batch_lambda_handler.py
@@ -1,16 +1,14 @@
 from common.logger_utility import *
 import boto3
 import json
-import uuid
 import time
 import os
 
 class CreateBatches:
-    pass
 
-    def __get_latest_batch(self, latest_batch_id):
+    def get_latest_batch(self, latest_batch_id):
         try:
-            ssm = boto3.client('ssm')
+            ssm = boto3.client('ssm', region_name='us-east-1')
             response = ssm.get_parameter(Name=latest_batch_id, WithDecryption=False)
             LoggerUtility.logInfo("Response from parameter store - {}".format(response))
             current_batch_id = response["Parameter"]["Value"]
@@ -19,26 +17,26 @@ class CreateBatches:
             raise ex
         return current_batch_id
     
-    def __create_new_batch_id(self, latest_batch_id):
-        new_batch_id=str(int(time.time()))
+    def create_new_batch_id(self, latest_batch_id):
+        new_batch_id = str(int(time.time()))
         try:
-            ssm = boto3.client('ssm')
+            ssm = boto3.client('ssm', region_name='us-east-1')
             response = ssm.put_parameter(
                 Name=latest_batch_id,
                 Description='Parameter to hold the latest value of a batch used for processing waze transactions',
                 Value=new_batch_id,
                 Type='String',
                 Overwrite=True,
-                AllowedPattern='\d+')
+                AllowedPattern='\\d+')
             LoggerUtility.logInfo("Successfully created a new batch with id - {}".format(new_batch_id))
         except Exception as ex:
             LoggerUtility.logError("Failed to create new batch with reason - {}".format(ex))
             raise ex
         return new_batch_id
     
-    def __push_batchid_to_queue(self, current_batch_id):
+    def push_batchid_to_queue(self, current_batch_id):
         try:
-            sqs = boto3.resource('sqs')
+            sqs = boto3.resource('sqs', region_name='us-east-1')
             queue_name = os.environ["SQS_CURATED_BATCHES_QUEUE_ARN"].rsplit(':', 1)[1]
             curated_batches_queue = sqs.get_queue_by_name(QueueName=queue_name)
             response = curated_batches_queue.send_message(MessageBody=json.dumps({
@@ -54,7 +52,7 @@ class CreateBatches:
         LoggerUtility.setLevel()
         LoggerUtility.logInfo("Initiating batch creation process")
         latest_batch_id = os.environ["LATEST_BATCH_ID"]
-        current_batch_id = self.__get_latest_batch(latest_batch_id)
+        current_batch_id = self.get_latest_batch(latest_batch_id)
         if "" == current_batch_id:
             new_batch_id = self.__create_new_batch_id(latest_batch_id)
         else:
