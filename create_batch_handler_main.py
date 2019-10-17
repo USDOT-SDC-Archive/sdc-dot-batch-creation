4b4fbce5b26d34bc9da4da16ee53932ffd6e5538
HannahKent <hkent@acrotron.com>
2019-10-17 17:21:02 -0600

    SDC-1005 - removed code smells - made pep8 compliant - optimized imports - swapped test order

--- create_batch_handler_main.py
+++ create_batch_handler_main.py
@@ -1,5 +1,5 @@
 from lambdas.create_batch_lambda_handler import *
 
-def lambda_handler(event, context):
+def lambda_handler(*args, **kwargs):
     create_batch_handle_event = CreateBatches()
-    create_batch_handle_event.create_batch(event, context)
+    create_batch_handle_event.create_batch()
